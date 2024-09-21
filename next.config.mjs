7ca87496afbcda0bdc3bb0070caa68d69e6c310e
kienhuynh279 <kienhuynh279@gmail.com>
2024-09-21 15:19:01 +0700

    add lintstage

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,24 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import bundleAnalyzer from '@next/bundle-analyzer';
+import createNextIntlPlugin from 'next-intl/plugin';
 
-export default nextConfig;
+const withNextIntl = createNextIntlPlugin('./src/i18n.tsx');
+
+const withBundleAnalyzer = bundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'standalone',
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
+};
+
+export default withBundleAnalyzer(withNextIntl({ ...nextConfig }));
