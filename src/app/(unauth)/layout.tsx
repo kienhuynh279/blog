5d744380b650537c8d08a2fffb6014edefe3d33b
kienhuynh279 <kienhuynh279@gmail.com>
2024-09-21 15:03:47 +0700

    add eslint

--- src/app/(unauth)/layout.tsx
+++ src/app/(unauth)/layout.tsx
@@ -7,9 +7,7 @@ type UnAuthLayoutProps = {
 const UnAuthLayout = ({ children }: UnAuthLayoutProps) => {
   return (
     <UnAuthWrapper>
-      <div className="flex h-screen w-screen items-center justify-center">
-        <div className="w-full max-w-[420px]">{children}</div>
-      </div>
+      <div>{children}</div>
     </UnAuthWrapper>
   );
 };
